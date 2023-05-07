91ce8b06f0f273f183eacdba69db818ec99650aa
kishore13-dotmaker <kishorem168@gmail.com>
2023-05-07 14:04:56 +0530

    History

--- controllers/findActions.js
+++ controllers/findActions.js
@@ -4,7 +4,9 @@ const authStringConstant = require("../constants/strings");
 const User = require("../models/user");
 const Employee = require("../models/employee");
 const httpStatusCode = require("../constants/httpStatusCodes");
-
+const moment = require("moment");
+const Checkin=require("../models/checkin");
+const Overtime=require("../models/overtime");
 const findActions = {
   findEmployee: async function (req, res) {
     try {
@@ -38,5 +40,69 @@ const findActions = {
       });
     }
   },
+findCheckins: async function (req, res) {
+    try {
+        const employeeId = req.body.employee_id;
+        const fromdate = req.body.fromdate;
+        const todate = req.body.todate;
+
+        const fromDate = new Date(moment(new Date(fromdate)).format("YYYY-MM-DD"));
+        const toDate = new Date(moment(new Date(todate)).format("YYYY-MM-DD"));
+
+        const employee = await Employee.findById(employeeId);
+        if (!employee) {
+            return res.status(httpStatusCode.UNAUTHORIZED).send({
+                success: false,
+                message: authStringConstant.USER_DOES_NOT_EXIST
+            });
+        }
+
+        const checkin = await Checkin.find({
+            employee: employeeId,
+            "checkin": { $gte: fromDate, $lte: toDate }
+        });
+
+        return res.status(httpStatusCode.OK).send({
+            checkin: checkin
+        });
+    } catch (err) {
+        return res.status(401).send({
+            err: err.message
+        });
+    }
+},
+findOvertime: async function (req, res) {
+    try {
+        const employeeId = req.body.employee_id;
+        const fromdate = req.body.fromdate;
+        const todate = req.body.todate;
+
+        const fromDate = new Date(moment(new Date(fromdate)).format("YYYY-MM-DD"));
+        const toDate = new Date(moment(new Date(todate)).format("YYYY-MM-DD"));
+
+        const employee = await Employee.findById(employeeId);
+        if (!employee) {
+            return res.status(httpStatusCode.UNAUTHORIZED).send({
+                success: false,
+                message: authStringConstant.USER_DOES_NOT_EXIST
+            });
+        }
+
+        const overtime = await Overtime.find({
+            employee: employeeId,
+            overtime: { $gte: fromDate, $lte: toDate }
+        }).exec();
+
+        return res.status(httpStatusCode.OK).send({
+            overtime: overtime
+        });
+    } catch (err) {
+        return res.status(401).send({
+            err: err.message
+        });
+    }
+},
+
+
 };
 module.exports = findActions;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const authActions = require("../controllers/authController");
 const adminActions = require("../controllers/adminController");
 const findActions = require("../controllers/findActions");
 const checkinActions = require("../controllers/checkinController");
+const moment = require("moment");
+
 // const auth = require("../middleware/auth");
 const app = express();
 
@@ -14,5 +16,7 @@ router.post("/renewAccessToken", authActions.renewAccessToken);
 router.post("/createEmployee", adminActions.addEmployee);
 router.get("/findEmployees", findActions.findEmployee);
 router.post("/checkin", checkinActions.newCheckin);
-router.post("/overtime",checkinActions.newOverTime)
+router.post("/overtime",checkinActions.newOverTime);
+router.post("/checkinHistory",findActions.findCheckins);
+router.post("/overtimeHistory",findActions.findOvertime);
 module.exports = router;
